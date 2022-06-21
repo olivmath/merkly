23b138b7b3cf2c4f96c4c53806d8ba10a017844e
Lucas Oliveira <olivmath@protonmail.com>
2022-06-21 18:36:51 -0300

    add: tests for merkle root

--- test/test_merkle_tree.py
+++ test/test_merkle_tree.py
@@ -2,6 +2,7 @@ from merkly.mtree import MerkleTree
 from pytest import raises, mark
 from typing import List
 
+
 def test_simple_merkle_tree_constructor():
   x = ["a", "b", "c", "d"]
   tree = MerkleTree(x)
@@ -13,15 +14,29 @@ def test_simple_merkle_tree_constructor():
     'f1918e8562236eb17adc8502332f4c9c82bc14e19bfc0aa10ab674ff75b3d2f3',
   ]
 
+
 @mark.parametrize(
   "x",
   [
-    ["a", "b", "c"],
-    ["a", "b", "c", "d", "e"],
-    ["a", "b", "c", "d", "e", "f"],
     ["a", "b", "c", "d", "e", "f", "g"],
+    ["a", "b", "c", "d", "e", "f"],
+    ["a", "b", "c", "d", "e"],
+    ["a", "b", "c"]
   ]
 )
 def test_error_simple_merkle_tree_constructor(x: List[str]):
   with raises(Exception):
     MerkleTree(x)
+
+
+@mark.parametrize(
+  "x, root",
+  [
+    (["a", "b", "c", "d", "e", "f", "g", "h"], "724655eedd83ac8b58d2d090f21a34755e5da82713332645e7db67aec88740ba"),
+    (["a", "b", "c", "d"], "f90854ce2337f197c3b0f047cecaae7d5bad1095ceb9c70070ca7793941efe4b"),
+    (["a", "b"], "e3b534d49837a4f8a2f95d5b71829e55a1708289654abbbf79c6441867555616")
+  ]
+)
+def test_simple_merkle_root(x: List[str], root: str):
+  tree = MerkleTree(x)
+  assert tree.root == root
--- test/utils/test_crypto.py
+++ test/utils/test_crypto.py
@@ -1,6 +1,6 @@
-from cmath import exp
-from merkly.utils.crypto import keccak
+from merkly.utils.crypto import keccak, slicer, merkle_root
 from pytest import mark
+from typing import List
 
 @mark.parametrize(
   "data, expect",
@@ -22,4 +22,34 @@ def test_of_keccak(data: str, expect: bool):
 
 
 
+@mark.parametrize(
+  "full, half",
+  [
+    ([i for i in range(2)], 1),
+    ([i for i in range(7)], 4),
+    ([i for i in range(10)], 5),
+    ([i for i in range(112)], 56),
+    ([i for i in range(200)], 100),
+    ([i for i in range(900)], 450),
+    ([i for i in range(1002)], 501),
+    ([i for i in range(2040)], 1020),
+    ([i for i in range(5008)], 2504),
+    ([i for i in range(10010)], 5005)
+  ]
+)
+def test_slicer(full: List[int], half: int):
+  assert half == len(slicer(full))
 
+
+@mark.parametrize(
+  "leafs, root",
+  [
+    (["a", "b", "c", "d", "e", "f", "g", "h", "1","2","3","4","5","6","7","8"], "3818695492749d7eefc6a22cde83b4276263e0597ec86f74bf6d8fc7423f721a"),
+    (["a", "b", "c", "d", "e", "f", "g", "h"], "5cf1a5ef31f067f883c20e9dd709b430ae273db5d65f61c1f175bbe7076d3206"),
+    (["a", "b", "c", "d"], "159b0d5005a27c97537ff0e6d1d0d619be408a5e3f2570816b02dc5a18b74f47"),
+    (["a", "b"], "63a9f18b64ca5a98ad9dba59259edb0710892614501480a9bed568d98450c151"),
+    (["a"], "3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb")
+  ]
+)
+def test_create_root(leafs: List[str], root: str):
+  assert merkle_root(leafs) == [root]
