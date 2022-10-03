e01f689b40d896bb8ec55fa1e79510a5e0328021
Lucas Oliveira <olivmath@protonmail.com>
2022-10-03 10:39:52 -0300

    add: scripts

---
+++ scripts/poetry.py
@@ -0,0 +1,16 @@
+import subprocess
+from rich import print
+
+
+def w_t():
+    """
+    # running `poetry run w-t` for watch tests
+    """
+    subprocess.run(["ptw", "test","-vvx"])
+
+
+def lint():
+    """
+    # running `poetry run lint` for run pylint
+    """
+    subprocess.run(["pylint", "merkly"])
