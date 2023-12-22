711ae702bfb1174bffc8b45b28025f296463e9ef
Lucas Oliveira <olivmath@protonmail.com>
2023-12-22 18:05:22 -0300

    fix: function of hash should have 2 args

--- test/merkletreejs/merkleTreeTest.py
+++ test/merkletreejs/merkleTreeTest.py
@@ -2,11 +2,12 @@ from merkly.mtree import MerkleTree
 import hashlib
 import json
 
-def sha256(data):
+def sha256(x, y):
+    data = x + y
     return hashlib.sha256(data.encode()).hexdigest()
 
 leaves = ["a", "b", "c", "d"]
 tree = MerkleTree(leaves, sha256, merkletreejs=True)
-root = tree.root()
+root = tree.root
 
 print(json.dumps({"merkleRoot": root}))
