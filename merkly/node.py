c0505009e3117d558c4e3e5ff031aeee5848b8cc
Lucas Oliveira <olivmath@protonmail.com>
2023-12-24 15:25:36 -0300

    add:  and  in node class

--- merkly/node.py
+++ merkly/node.py
@@ -13,5 +13,11 @@ class Node(BaseModel):
     # 🍃 Leaf of Merkle Tree
     """
 
-    data: Optional[str] = None
+    data: Optional[bytes] = None
     side: Side = Side.LEFT
+
+    def __eq__(self, other: "Node") -> bool:
+        return self.data == other.data
+
+    def __repr__(self) -> str:
+        return f"Node({self.data.hex()}, {self.side})"
