8c61d3bacd954c01f896df4f9e6f9ab367a73d93
Lucas Oliveira <olivmath@protonmail.com>
2023-12-24 15:19:31 -0300

    add: tests for compatibility in merkle root

---
+++ test/merkletreejs/merkle_root/merkle_root_test.py
@@ -0,0 +1,13 @@
+from merkly.mtree import MerkleTree
+import hashlib
+import json
+
+def sha256(x, y):
+    data = x + y
+    return hashlib.sha256(data).digest()
+
+leaves = ["a", "b", "c", "d"]
+tree = MerkleTree(leaves, sha256)
+root = tree.root.hex()
+
+print(json.dumps({"root": root}))
