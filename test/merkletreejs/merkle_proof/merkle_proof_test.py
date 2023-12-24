a085e583ef5ab094e8467643f245de5354567075
Lucas Oliveira <olivmath@protonmail.com>
2023-12-24 15:19:44 -0300

    add: tests for compatibility in merkle proof

---
+++ test/merkletreejs/merkle_proof/merkle_proof_test.py
@@ -0,0 +1,15 @@
+from merkly.mtree import MerkleTree
+import hashlib
+import json
+
+def sha256(x, y):
+    data = x + y
+    return hashlib.sha256(data).digest()
+
+leaves = ["a", "b", "c", "d", "e", "f", "g", "h"]
+tree = MerkleTree(leaves, sha256)
+leaf = 'a'
+proof = tree.proof(leaf)
+formatted_proof = [{"data": node.data.hex(), "position": node.side.name.lower()} for node in proof]
+
+print(json.dumps({"proof": formatted_proof, "isValid": tree.verify(proof, leaf)}))
