0b843e6aa135131a2bb5b7e22d5e9055a070c29b
Lucas Oliveira <olivmath@protonmail.com>
2024-02-01 01:51:45 -0300

    fix: merkle tree js tests

--- test/conftest.py
+++ test/conftest.py
@@ -8,3 +8,10 @@ def compile_rust_ffi(request):
     if any("ffi" in item.keywords for item in request.session.items):
         print("Compiling Rust FFI")
         subprocess.run(["./build.sh"], cwd="./merkly/accelerator", check=True)
+
+
+@fixture(scope="session", autouse=True)
+def install_js_deps(request):
+    if any("merkletreejs" in item.keywords for item in request.session.items):
+        print("Install js dependencies")
+        subprocess.run(["yarn"], cwd="./test/merkletreejs", check=True)
