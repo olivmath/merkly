d8696d6a5d22eed1f7fcdbeac6d4df6a5968216f
Lucas Oliveira <olivmath@protonmail.com>
2024-02-02 01:41:19 -0300

    update: tests

--- test/accelerator/test_accelerator.py
+++ test/accelerator/test_accelerator.py
@@ -1,6 +1,7 @@
 from merkly.accelerator.mtreers import MTreers
 from merkly.mtree import MerkleTree
 from keccaky import hash_it_bytes
+from merkly.node import Node
 from pytest import mark
 
 
@@ -16,3 +17,30 @@ def test_merkle_root_ffi(compile_rust_ffi):
     result = treers.make_root(leaves_bytes)
 
     assert list(root) == list(result)
+
+
+@mark.ffi
+def test_merkle_proof_ffi(compile_rust_ffi):
+    leaves_short = ["a", "b", "c", "d"]
+    leaves_bytes = list(map(lambda x: hash_it_bytes(x.encode()), leaves_short))
+
+    tree = MerkleTree(leaves_short)
+    treers = MTreers()
+
+    proof = tree.proof("a")
+    result = treers.make_proof(leaves_bytes, leaves_bytes[0])
+    assert proof == [
+        Node(
+            data=bytes.fromhex(
+                "b5553de315e0edf504d9150af82dafa5c4667fa618ed0a6f19c69b41166c5510"
+            ),
+            side=1,
+        ),
+        Node(
+            data=bytes.fromhex(
+                "d253a52d4cb00de2895e85f2529e2976e6aaaa5c18106b68ab66813e14415669"
+            ),
+            side=1,
+        ),
+    ]
+    assert result == []
